3b369208553a426be710faf4b0ed83ea02b4fd70
Thato <t.chitja369@gmail.com>
2023-10-09 11:09:12 +0200

    Update product model

--- Winehouse/Winehouse/Models/Product.cs
+++ Winehouse/Winehouse/Models/Product.cs
@@ -4,13 +4,13 @@
     {
         public int ProductId { get; set; }
 
-        public string Product_Name { get; set; }
+        public string Product_Name { get; set; } = string.Empty;
 
-        public string Product_Category { get; set; }
+        public string Product_Description { get; set; } = string.Empty;
 
-        public string Product_Brand { get; set; }
+        public string? Product_Category { get; set; }
 
-        public string Product_Description { get; set; }
+        public string Product_Brand { get; set; } = string.Empty;
 
         public int Product_Price { get; set; } = 0;
     }
