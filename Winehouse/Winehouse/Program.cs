fc4437823b439c1f8decc097dbb77cb2aaaa1e89
Thato <t.chitja369@gmail.com>
2023-10-09 11:37:51 +0200

    Register the DbContext

--- Winehouse/Winehouse/Program.cs
+++ Winehouse/Winehouse/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Winehouse.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<StoreContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+));
 
 var app = builder.Build();
 
