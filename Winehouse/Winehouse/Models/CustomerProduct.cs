f94a863bae7f1564fb40f1fc195aa31eda1249fe
Thato <t.chitja369@gmail.com>
2023-10-09 11:14:18 +0200

    Add CustomerProduct model

---
+++ Winehouse/Winehouse/Models/CustomerProduct.cs
@@ -0,0 +1,14 @@
+﻿namespace Winehouse.Models
+{
+    public class CustomerProduct
+    {
+        public int CustomerProductId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+        public virtual ICollection<Customer> Customers { get; set; }
+    }
+}
