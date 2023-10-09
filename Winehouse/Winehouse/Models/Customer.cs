43ee040aa017d399c6e3ca78a7cc89dc05abc6bf
Thato <t.chitja369@gmail.com>
2023-10-09 11:11:56 +0200

    Add Customer model

---
+++ Winehouse/Winehouse/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Winehouse.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public string? CustomerEmail { get; set; }
+    }
+}
